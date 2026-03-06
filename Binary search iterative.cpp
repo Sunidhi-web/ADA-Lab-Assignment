ce35a08571c13b4fe635d4d86e91612af5306bea
Sunidhi <sunidhisahni.9@gmail.com>
2026-03-06 11:52:30 +0530

    Implement iterative binary search algorithm

--- Binary search iterative.cpp
+++ Binary search iterative.cpp
@@ -1,1 +1,28 @@
+#include <iostream>
+#include <vector>
+using namespace std;
 
+int binarySearch(vector<int> arr, int tar){
+	int st = 0, end = arr.size() - 1;
+	
+	while(st <= end){
+		int mid = (st + end) / 2;
+		if(tar > arr[mid]){
+			st = mid + 1;
+		}else if(tar < arr[mid]){
+			end = mid - 1;
+		}else{
+			return mid;
+		}
+	}
+	return -1;
+}
+
+int main(){
+	vector<int> arr1 = {-1, 0, 3, 4, 5, 9, 12};  //odd array
+	
+	
+	vector<int> arr2 = {-1, 0, 3,5, 9, 12};     //even array
+	int tar2 = 5;
+	cout << binarySearch(arr2, tar2) << endl;
+}
